d6dee04419a7c05a6a2ab1b7d70af3f2d7f79a32
hland <hland@DESKTOP-BKOJVN6>
2018-07-25 16:56:29 -0300

    start Maven project

---
+++ src/test/java/br/com/hubtech/contasapi/ContasApiApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.hubtech.contasapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContasApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
